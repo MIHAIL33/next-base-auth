c2bf3021de27644dbee73fff5585ceeaf4d92904
vasilev.mihail21 <vasilev.mihail21@wb.ru>
2022-10-13 09:32:26 +0300

    feat: base struct

---
+++ client/pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <>
+            <h1 className="jumbotron text-center bg-primary square">Marketplace</h1>
+        </>
+    );
+};
+
+export default Index;
