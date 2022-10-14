6b44964bd67c4cdb28acf4906a7149ef18a8e429
vasilev.mihail21 <vasilev.mihail21@wb.ru>
2022-10-14 23:28:22 +0300

    feat: add register and login page

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,12 +1,13 @@
 import User from '../models/user';
 import { hashPassword, comparePassword } from '../utils/auth';
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
         if (!name) return res.status(400).send("Name is required");
-        if (!password || password.lenght < 3) {
+        if (!password || password.length < 3) {
             return res
                 .status(400)
                 .send("Password is required and should be min 3 characters long");
@@ -28,3 +29,28 @@ export const register = async (req, res) => {
         return res.status(400).send('Error. Try again');
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email }).exec()
+        if (!user) return res.status(400).send("No user found")
+
+        const match = await comparePassword(password, user.password)
+        if (!match) return res.status(400).send("Wrong password")
+
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        })
+
+        user.password = undefined
+        res.cookie('token', token, {
+            httpOnly: true,
+            // secure: true,
+        })
+        res.json(user)
+    } catch (err) {
+        console.log(err)
+        return res.status(400).send("Error. Try again.")
+    } 
+}
